51ef4b2c9322f0aaad463657929a60dea2d6b559
Missingrakan <3047451493@qq.com>
2019-11-05 22:34:30 +0800

    更新代码

--- task1/task1/test.c
+++ task1/task1/test.c
@@ -9,69 +9,193 @@
 #define DataType int
 #define SEQLIST_DEFAULT_SIZE 6
 
-typedef struct SeqList
-{
-	DataType *base;
-	size_t capacity;
-	size_t size;
-}SeqList;
+//typedef struct SeqList
+//{
+//	DataType *base;
+//	size_t capacity;
+//	size_t size;
+//}SeqList;
+//
+//void swap(DataType *elem1, DataType *elem2)
+//{
+//	DataType tmp = *elem1;
+//	*elem1 = *elem2;
+//	*elem2 = tmp;
+//}
+// 
+//
+//void SeqListReverse(SeqList *psl)
+//{
+//	int begin = 0;
+//	int end = psl->size - 1;
+//	while (begin < end)
+//	{
+//		swap(psl->base[begin], psl->base[end]);
+//		begin++;
+//		end--;
+//	}
+//}
+//
+//void SeqListInit(SeqList *psl,int sz)
+//{
+//	psl->capacity = sz > SEQLIST_DEFAULT_SIZE ? sz : SEQLIST_DEFAULT_SIZE;
+//	psl->base = (DataType*)malloc(sizeof(DataType)*(psl->capacity));
+//	psl->size = 0;
+//}
+//
+//void SeqListShow(SeqList *psl)
+//{
+//	printf("SeqList:");
+//	for (size_t i = 0; i < psl->size; i++)
+//	{
+//		printf("%d ", psl->base[i]);
+//	}
+//	printf("\n");
+//}
+//
+//int SeqListIsFull(SeqList *psl)
+//{
+//	return psl->size >= psl->capacity;
+//}
+//
+//int SeqListPushBack(SeqList *psl, DataType x)
+//{
+//	if (SeqListIsFull(psl))
+//	{
+//		printf("push_back:顺序表已满，%d不能插入!\n", x);
+//		return -1;
+//	}
+//	psl->base[psl->size++] = x;
+//	return 1;
+//}
+//
+//int main()
+//{
+//	SeqList mylist;
+//	SeqListInit(&mylist, SEQLIST_DEFAULT_SIZE);
+//
+//}
 
-void swap(DataType *elem1, DataType *elem2)
-{
-	DataType tmp = *elem1;
-	*elem1 = *elem2;
-	*elem2 = tmp;
-}
- 
 
-void SeqListReverse(SeqList *psl)
+//void main()
+//{
+//	int a = 0;
+//	char c = 0;
+//	scanf("%3d,%3c",&a,&c);
+//	printf("%d,%c",a, c);
+//}
+
+
+#define DataType int
+
+typedef struct node
 {
-	int begin = 0;
-	int end = psl->size - 1;
-	while (begin < end)
+	DataType data;
+	struct node *next;
+}LNode;
+
+typedef LNode* SList;
+
+LNode* CreatLinkListByTail();
+LNode* Get_LinkList(LNode* head, int k);
+void LinkListShow(LNode* head);
+
+void SListInit(SList *plist)
+{
+	LNode* s;
+	int  i = 0;
+	*plist = (LNode*)malloc(sizeof(LNode));
+	(*plist)->next = NULL;
+	while (i<=10)
 	{
-		swap(psl->base[begin], psl->base[end]);
-		begin++;
-		end--;
+		s = (LNode*)malloc(sizeof(LNode));
+		s->data = 0;
+		s->next = (*plist)->next;
+		(*plist)->next = s;
+		i++;
 	}
+	
 }
 
-void SeqListInit(SeqList *psl,int sz)
+void LinkListShow(LNode *head)
 {
-	psl->capacity = sz > SEQLIST_DEFAULT_SIZE ? sz : SEQLIST_DEFAULT_SIZE;
-	psl->base = (DataType*)malloc(sizeof(DataType)*(psl->capacity));
-	psl->size = 0;
-}
-
-void SeqListShow(SeqList *psl)
-{
-	printf("SeqList:");
-	for (size_t i = 0; i < psl->size; i++)
+	LNode *p = head->next;
+	if (p == NULL)
+		return;
+	while (p != NULL)
 	{
-		printf("%d ", psl->base[i]);
+		if (p->data != 0)
+			printf("%d->", p->data);
+		p = p->next;
 	}
-	printf("\n");
+	printf("Over!\n");
 }
 
-int SeqListIsFull(SeqList *psl)
+LNode* CreatLinkListByHead()
 {
-	return psl->size >= psl->capacity;
-}
+	LNode *s;
+	int x;
+	LNode* head;
+	head = (LNode*)malloc(sizeof(LNode));
+	head->data = 0;
+	head->next = NULL;
 
-int SeqListPushBack(SeqList *psl, DataType x)
-{
-	if (SeqListIsFull(psl))
+	printf("请输入数值(以-1结束)");
+	scanf("%d", &x);
+	while (x != -1)
 	{
-		printf("push_back:顺序表已满，%d不能插入!\n", x);
-		return -1;
+		s = (LNode*)malloc(sizeof(LNode));
+		s->data = x;
+		s->next = head->next;
+		head->next = s;
+		scanf("%d", &x);
 	}
-	psl->base[psl->size++] = x;
-	return 1;
+
+	return head;
 }
+
+//void LinkListSort(LNode* plist)
+//{
+//
+//}
+
+void LinkListDeverse(LNode* plist,LNode* listodd,LNode *listeven)
+{
+	if (plist == NULL)
+		return;
+	//while(plist->next == NULL)
+	while (plist != NULL)
+	{
+		if (plist->data % 2 != 0)
+			listodd->data = plist->data;
+		else
+			listeven->data = plist->data;
+		plist = plist->next;
+		listodd = listodd->next;
+		listeven = listeven->next;
+	}
+	/*LinkListSort(listodd);
+	LinkListSort(listeven);*/
+}
+
 
 int main()
 {
-	SeqList mylist;
-	SeqListInit(&mylist, SEQLIST_DEFAULT_SIZE);
+	LNode *plist = NULL;
+	LNode *listeven = NULL;
+	LNode *listodd = NULL;
+	SListInit(&listodd);
+	SListInit(&listeven);
 
+	plist = CreatLinkListByHead();
+	printf("plist：");
+	LinkListShow(plist);
+	LinkListDeverse(plist, listodd, listeven);
+	printf("listodd：");
+	LinkListShow(listodd);
+	printf("listeven：");
+	LinkListShow(listeven);
+
+
+	return 0;
 }
